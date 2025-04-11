46a43c78adf17d95a9d286aca6202ca8add54cd4
shab <dirtyshab@protonmail.com>
2025-04-10 23:18:47 -0400

    export useAuthenticationStatus()

--- packages/rainbowkit/src/index.ts
+++ packages/rainbowkit/src/index.ts
@@ -36,4 +36,4 @@ export { midnightTheme } from './themes/midnightTheme';
 export { cssStringFromTheme } from './css/cssStringFromTheme';
 export { cssObjectFromTheme } from './css/cssObjectFromTheme';
 export { __private__ } from './__private__';
-export { useAuthenticationAdapter } from './components/RainbowKitProvider/AuthenticationContext';
+export { useAuthenticationAdapter, useAuthenticationStatus } from './components/RainbowKitProvider/AuthenticationContext';
